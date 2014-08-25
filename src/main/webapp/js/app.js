b610ace95d83e513a9314c9944ba91f3eafee420
funkomatic <cdr53x@free.fr>
2014-08-25 11:02:27 +0200

    Cleanup of some angular variables, fixed typo in list

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -6,8 +6,6 @@
 
       this.categories = [];
       this.players = [];
-      this.selcat = false;
-
       var myself = this;
 
       /** get the categories */
@@ -17,7 +15,6 @@
 
       /** update the players for a given category */
       this.updatePlayers=function(selectedCategory)   {
-	myself.selcat=true;
 	$http.get('./api/v1/players/'+selectedCategory.value).success(function(data) {
 	myself.players=data;
       });
